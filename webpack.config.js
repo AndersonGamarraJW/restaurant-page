1a1b238bc70a636a2c14b476d88a7db0a2bdf33a
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-02-20 18:43:04 -0500

    Modifica la configuracion de webpack para eliminar limpieza

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
  entry: './src/index.js',
  output: {
    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean:true,
+    path: path.resolve(__dirname, 'dist')
   },
 };
