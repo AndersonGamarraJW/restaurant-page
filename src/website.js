e60f4796c8ecec8aab21a436dcc1cb6967e66e3e
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-02-20 20:37:08 -0500

    feat(website): Implemente funcion que crea el nav

--- src/website.js
+++ src/website.js
@@ -1,3 +1,19 @@
-function createWeabsiteStructure(){
+function createNav(options){
+    const nav = document.createElement('nav');
+    const listUl = document.createElement('ul');
+    options.forEach(element => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = element;
+        li.appendChild(a);
+        listUl.appendChild(li);
+    });
+    nav.appendChild(listUl);
+    return nav;
+}
+function createHeader(){
+    const header = document.createElement('header');
+    const title = document.createElement('h1');
+    title.textContent = "Peruvian Restaurant";
     
 }
