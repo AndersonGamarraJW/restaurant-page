b6417457ebcc8e0ff0883cb79ce0997ca6529e7d
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-02-20 20:03:23 -0500

    feat(menu): Crea el contenido de la pantalla Menu

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,35 @@
+import { Menu } from "./menu-object.js";
 function createMenuContainer(){
     const mainContainer = document.createElement('div');
     mainContainer.setAttribute('id','menu-container');
     
+    mainContainer.appendChild(createMenuItemContainer(new Menu('Lomo Saltado','Plato tipico del Peru ','01 Hrs')));
+    mainContainer.appendChild(createMenuItemContainer(new Menu('Ceviche','Plato tipico del Peru','20 Min')));
+    mainContainer.appendChild(createMenuItemContainer(new Menu('Papa con huevo','Snack tipico del Peru','10 Min')));
+    
+    return mainContainer;
 }
 
-function createMenu(info){
+function createMenuItemContainer(menu){
+    const menuItemContainer = document.createElement('div');
+    menuItemContainer.classList.add('menu-item-container');
+    const titleMenu = document.createElement('p');
+    titleMenu.textContent = menu.title;
     
+    const descMenu = document.createElement('p');
+    descMenu.textContent = menu.desc;
+
+    const timeMenu = document.createElement('p');
+    descMenu.textContent = 'Time: ' + menu.time;
+
+    menuItemContainer.appendChild(titleMenu);
+    menuItemContainer.appendChild(descMenu);
+    menuItemContainer.appendChild(timeMenu);
+    
+    return menuItemContainer;
 }
+function loadMenu(){
+    const main = document.querySelector('main');
+    main.appendChild(createMenuContainer());
+}
+export default loadMenu;
